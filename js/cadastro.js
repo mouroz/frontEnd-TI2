9696b72c7f185b68406ddd39a87711831e770a28
ebolispace <beboli@sga.pucminas.br>
2023-09-20 16:26:34 -0300

    Adicionando as páginas HTML

---
+++ js/cadastro.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const botaoEntrar = document.getElementById("Proximo");
+
+    botaoEntrar.addEventListener("click", function() {
+        window.location.href = "./Outras paginas/cadastro2.html";
+    });
+});
---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const botaoEntrar = document.getElementById("botaoCadastrar");
+
+    botaoEntrar.addEventListener("click", function() {
+        window.location.href = "./Outras paginas/cadastro.html";
+    });
+});
