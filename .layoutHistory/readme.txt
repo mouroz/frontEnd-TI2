5350d5ad978b4732673c2a4d841de660a7134d17
gabriel mourao <gabrielmourao265@gmail.com>
2023-11-18 23:52:45 -0300

    Reorganized html for proper modularization and documentation

--- .layoutHistory/readme.txt
+++
@@ -1,3 +0,0 @@
-there are multiple possible themes and skins that could make the website different,
-from different headers to different css for those headers. This folder carries on
-those old models, in case something needs reverted back
--- js/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const botaoEntrar = document.getElementById("botaoCadastrar");
-
-    botaoEntrar.addEventListener("click", function() {
-        window.location.href = "./outras/cadastro.html";
-    });
-});
